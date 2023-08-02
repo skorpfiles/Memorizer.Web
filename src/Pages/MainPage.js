01bbae7caf4a4c487915032806004f065413296f
Skorp24 <skorpfiles@gmail.com>
2023-08-02 08:40:36 +0200

    test env

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 function MainPage() {
     useEffect(() => {
         document.title = "Memorizer";
+        console.log(process.env.REACT_APP_MANAGEMENT_URL);
     });
 
     return (
