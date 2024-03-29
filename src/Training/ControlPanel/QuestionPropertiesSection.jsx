8cdf29d906e3f0709d7fee061c521729d005d7c7
Skorp24 <skorpfiles@gmail.com>
2024-03-29 13:52:50 +0100

    fixed the problem with Questionnaire section

--- src/Training/ControlPanel/QuestionPropertiesSection.jsx
+++ src/Training/ControlPanel/QuestionPropertiesSection.jsx
@@ -3,8 +3,10 @@ import Stats from './QuestionPropertiesSection/Stats';
 import styles from './QuestionPropertiesSection.module.css';
 function QuestionPropertiesSection() {
     return (
-        <div className={`font--default ${styles['content']}`} style={{"display":"flex", "flexDirection":"row"}} >
-            <Questionnaire name="Piano Exercises Piano Exercises Piano Exercises Piano Exercises Piano Exercises " />
+        <div className={`font--default row ${styles['container']}`} >
+            <div className={styles['questionnaire-container']}>
+                <Questionnaire name="Piano Exercises Piano Exercises Piano Exercises Piano Exercises Piano Exercises " />
+            </div>
             <div className={styles['separator']} />
             <Stats/>
         </div>
--- src/Training/ControlPanel/QuestionPropertiesSection/Questionnaire.jsx
+++ src/Training/ControlPanel/QuestionPropertiesSection/Questionnaire.jsx
@@ -2,7 +2,7 @@ import bookIcon from './book.png';
 import iconAndValueStyles from '../IconAndValue.module.css';
 function Questionnaire(props) {
     return (
-        <div className='row' style={{"width":"100%"}}>
+        <div className='row'>
             <img src={bookIcon} width='16rem' alt='Questionnaire' title='Questionnaire' />
             <div className={`overflow-ellipsis ${iconAndValueStyles['value']}`}>{props.name}</div>
         </div>
