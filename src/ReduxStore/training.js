c17086b1b0c3b14d7a943d4b33292324b95b070e
Skorp24 <skorpfiles@gmail.com>
2024-04-22 09:39:51 +0200

    adding getting questions list, work in process

--- src/ReduxStore/training.js
+++ src/ReduxStore/training.js
@@ -3,49 +3,49 @@ import { createSlice } from '@reduxjs/toolkit';
 const trainingStateSlice = createSlice({
     name: 'trainingStateSlice',
     initialState: {
-        trainingQuestions: {
-            areLoading: false,
-            loadingFinished: false,
-            loadingSucceed: false,
-            items: null,
-            itemsCount: null
-        },
-        currentState: {
-            question: null,
-            questionIndex: null,
-            trainingStage: null,
-            trainingStageParameters: null,
-            questionTimeSeconds: 0,
-            correctAnswersPercent: 100,
-            isTrainingResultReady: false
-        },
-        uploadingResults: {
-            areUploading: false,
-            finished: false,
-            succeed: false
-        }
+        //trainingQuestions: {
+        //    areLoading: false,
+        //    loadingFinished: false,
+        //    loadingSucceed: false,
+        //    items: null,
+        //    itemsCount: null
+        //},
+        trainingId: null,
+        questions: null,
+        questionsCount: null,
+        currentQuestion: null,
+        currentQuestionIndex: null,
+        trainingStage: null,
+        trainingStageParameters: null,
+        currentQuestionTimeSeconds: 0,
+        correctAnswersPercent: 100,
+        isTrainingResultReady: false
+        //uploadingResults: {
+        //    areUploading: false,
+        //    finished: false,
+        //    succeed: false
+        //}
     },
     reducers: {
         setDefault(state) {
-            state.trainingQuestions.areLoading = false;
-            state.trainingQuestions.loadingFinished = false;
-            state.trainingQuestions.loadingSucceed = false;
-            state.trainingQuestions.items = null;
-            state.trainingQuestions.itemsCount = null;
-
-            state.currentState.question = null;
-            state.currentState.questionIndex = null;
-            state.currentState.trainingStage = null;
-            state.currentState.trainingStageParameters = null;
-            state.currentState.questionTimeSeconds = 0;
-            state.currentState.correctAnswersPercent = 100;
-            state.currentState.isTrainingResultReady = false;
-
-            state.uploadingResults.areUploading = false;
-            state.uploadingResults.finished = false;
-            state.uploadingResults.succeed = false;
+            state.trainingId = null;
+            state.questions = null;
+            state.questionsCount = null;
+            state.currentQuestion = null;
+            state.currentQuestionIndex = null;
+            state.trainingStage = null;
+            state.trainingStageParameters = null;
+            state.currentQuestionTimeSeconds = 0;
+            state.correctAnswersPercent = 100;
+            state.isTrainingResultReady = false;
         },
-
+        setTrainingId(state, action) {
+            state.trainingId = action.payload
+        },
+        setNewQuestionsList(state, action) {
+            state.questions = action.payload.questions;
+            state.questionsCount = action.payload.questions.length;
+        }
     }
 });
 
--- src/RouteElements/TrainingRouteElement.jsx
+++ src/RouteElements/TrainingRouteElement.jsx
@@ -3,13 +3,93 @@ import TrainingPage from '../Pages/Training/TrainingPage';
 import TrainingResultPage from '../Pages/Training/TrainingResultPage';
 import TrainingLoadingPage from '../Pages/Training/TrainingLoadingPage';
 import { useWallpaperViewDispatcher } from '../hooks/useWallpaperViewDispatcher';
-import { useEffect } from 'react';
-function TrainingRouteElement() {
+import { useState, useEffect, useReducer } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { callApi } from '../Utils/GlobalUtils';
+import { trainingActions } from '../ReduxStore/training';
+
+function TrainingRouteElement(props) {
     const setWallpaperView = useWallpaperViewDispatcher();
+
+    const questionsForTrainingReducer = (state, action) => {
+        switch (action.type) {
+            case 'setIsLoading':
+                return {
+                    isLoading: true,
+                    loadingFinished: false,
+                    loadingSucceed: false,
+                    loadingError: false,
+                    loadingErrorMessage: null
+                };
+            case 'setSuccess':
+                return {
+                    isLoading: false,
+                    loadingFinished: true,
+                    loadingSucceed: true,
+                    loadingError: false,
+                    loadingErrorMessage: null
+                };
+            case 'setError':
+                return {
+                    isLoading: false,
+                    loadingFinished: true,
+                    loadingSucceed: false,
+                    loadingError: true,
+                    loadingErrorMessage: action.errorMessage
+                };
+            default:
+                return { ...state };
+        }
+    }
+
+    const [isTrainingIdCorrect, setIsTrainingIdCorrect] = useState(null);
+
+    const [questionsForTrainingState, dispatchQuestionsForTrainingState] = useReducer(questionsForTrainingReducer, {
+        isLoading: false,
+        loadingFinished: false,
+        loadingSucceed: false,
+        loadingError: false,
+        loadingErrorMessage: null
+    });
+
+    const accessToken = useSelector(state => state.user.accessToken);
+
+    const dispatch = useDispatch();
 
     useEffect(() => {
         setWallpaperView('trainingWallpaper');
     }, [setWallpaperView]);
+
+    useEffect(() => {
+        if (props.trainingId !== null) {
+            setIsTrainingIdCorrect(true);
+            dispatch(trainingActions.setDefault());
+            dispatchQuestionsForTrainingState('setIsLoading');
+            try {
+                const loadingQuestionsListFunc = async () => {
+                    const url = '/Repository/Questionnaires/' + props.trainingId;
+                    const response = await callApi(url, 'GET', accessToken);
+                    if (response.ok) {
+                        const result = await response.json();
+                        dispatch(trainingActions.setTrainingId(props.trainingId));
+                    }
+                }
+
+
+
+
+
+
+
+
+                dispatch(trainingActions.setNewQuestionsList());
+            }
+            catch(error) {
+                console.log(error);
+                dispatchQuestionsForTrainingState('setError', 'Error: Unable to connect to the API.');
+            }
+        }
+    })
 
     return (
         <div className='route-element-with-return-button'>
