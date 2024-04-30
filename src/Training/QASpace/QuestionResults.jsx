ee90106ba890d55b1006da30e5dab3c240e0c300
Skorp24 <skorpfiles@gmail.com>
2024-04-30 22:41:14 +0200

    fixed some components

--- src/Training/QASpace/QuestionResults.jsx
+++ src/Training/QASpace/QuestionResults.jsx
@@ -1,24 +1,20 @@
 import QuestionIcon from './question.png';
 import styles from './QuestionResults.module.css';
-function QuestionResults(props) {
-    let typedAnswersMode = props.questionType === 'typedAnswers';
-    let htmlAnswers = [];
+import { useSelector } from 'react-redux';
+function QuestionResults() {
+    const questionType = useSelector(state => state.trainingState.questions[state.trainingState.currentQuestionIndex].type);
+    const trainingStageParameters = useSelector(state => state.trainingState.trainingStageParameters);
+    const questionText = useSelector(state => state.trainingState.questions[state.trainingState.currentQuestionIndex].text);
+    const untypedAnswer = useSelector(state => state.trainingState.questions[state.trainingState.currentQuestionIndex].untypedAnswer);
+    const typedAnswers = useSelector(state => state.trainingState.questions[state.trainingState.currentQuestionIndex].typedAnswers);
+
+    const typedAnswersMode = questionType === 'typedAnswers';
+    const htmlAnswers = [];
 
     if (typedAnswersMode) {
-        const words = [
-            {
-                text: 'stol',
-                isCorrect: false
-            },
-            {
-                text: 'tablica',
-                isCorrect: true
-            }
-        ];
-
-        for (let i = 0; i < words.length; i++) {
-            htmlAnswers.push(words[i].isCorrect ? (<span>{words[i].text}</span>) : (<span style={{ "color": "red" }}>{words[i].text}</span>));
-            if (i < words.length - 1) {
+        for (let i = 0; i < typedAnswers.length; i++) {
+            htmlAnswers.push(typedAnswers[i].isCorrect ? (<span>{typedAnswers[i].text}</span>) : (<span style={{ "color": "red" }}>{typedAnswers[i].text}</span>));
+            if (i < typedAnswers.length - 1) {
                 htmlAnswers.push('; ');
             }
         }
@@ -28,13 +24,13 @@ function QuestionResults(props) {
         <div className={`column ${styles['container']}`}>
             <div className={`row ${styles['question-container']}`}>
                 <img src={QuestionIcon} width='24rem' alt='Question' title='Question' />
-                <div className={`font--question-above-answer ${styles['question-text']}`}>table</div>
+                <div className={`font--question-above-answer ${styles['question-text']}`}>{questionText}</div>
             </div>
             <div className={`font--main-for-training-questions border-radius-small ${styles['answer']}`}>{typedAnswersMode ?
                 htmlAnswers.map(ans => ans) :
-                'Second conditional sentences use a past tense in the "if" clause and would + infinitive in the main clause.'}</div>
-            {(typedAnswersMode && props.typedAnswersCheckResultMode === 'incorrect') && (<div className={`font--default ${styles['incorrect-result-message']}`}>You have some mistakes.<br />The question will be retrained in the next trainings.</div>)}
-            {(typedAnswersMode && props.typedAnswersCheckResultMode === 'correct') && (<div className={`font--main-for-training-questions ${styles['correct-result-message']}`}>correct</div>)}
+                untypedAnswer}</div>
+            {(typedAnswersMode && trainingStageParameters[0] === 'incorrect') && (<div className={`font--default ${styles['incorrect-result-message']}`}>You have some mistakes.<br />The question will be retrained in the next trainings.</div>)}
+            {(typedAnswersMode && trainingStageParameters[0] === 'correct') && (<div className={`font--main-for-training-questions ${styles['correct-result-message']}`}>correct</div>)}
         </div>
     );
 }
--- src/Training/QASpace/QuestionWithTypingAnswer.jsx
+++ src/Training/QASpace/QuestionWithTypingAnswer.jsx
@@ -1,10 +1,14 @@
 import TypingAnswerPanel from './TypingAnswerPanel';
 import styles from './QuestionWithTypingAnswer.module.css';
+import { useSelector } from 'react-redux';
+
 function QuestionWithTypingAnswer() {
+    const questionText = useSelector(state => state.trainingState.questions[state.trainingState.currentQuestionIndex].text);
+
     return (
         <div className={`column ${styles['outer-container']}`}>
             <div className={`column ${styles['inner-container']}`}>
-                <div className={`font--main-for-training-questions ${styles['question']}`}>table</div>
+                <div className={`font--main-for-training-questions ${styles['question']}`}>{questionText}</div>
                 <div className='column full-width'>
                     <TypingAnswerPanel />
                 </div>
