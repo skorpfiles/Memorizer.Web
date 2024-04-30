4be289ae6da58fdd9c77f4b664b46110145ede81
Skorp24 <skorpfiles@gmail.com>
2024-04-30 21:04:53 +0200

    finalized displaying of training stages on the control panel

--- src/Training/ControlPanel/QuestionStageSection.jsx
+++ src/Training/ControlPanel/QuestionStageSection.jsx
@@ -7,15 +7,65 @@ import { useSelector } from 'react-redux';
 function QuestionStageSection() {
     const trainingStage = useSelector(state => state.trainingState.trainingStage);
     const isQuestionNew = useSelector(state => state.trainingState.currentQuestion.myStatus.isNew);
+    const questionType = useSelector(state => state.trainingState.currentQuestion.type);
+
+    const trainingStagesInfoForShowing = getTitlesOfTrainingStagesOnQuestionType(questionType);
+    const trainingStagesInfoForCurrentNewStatus = isQuestionNew
+        ? trainingStagesInfoForShowing.filter(element => element.showForNew)
+        : trainingStagesInfoForShowing.filter(element => element.showForOld);
 
     return (
         <div className={`row ${styles['content']}`}>
             {isQuestionNew && (<NewQuestionSignal />)}
-            <Stage name="Learn" isActive={ (trainingStage === 'learn') } />
-            <Stage name="Train" isActive={ (trainingStage === 'train') } />
-            <Stage name="Check" isActive={ (trainingStage === 'check') } />
+            {trainingStagesInfoForCurrentNewStatus.map(element => (
+                <Stage key={element.title} name={element.title} isActive={element.trainingStages.includes(trainingStage)}/>
+            ))}
         </div>
     )
+}
+
+function getTitlesOfTrainingStagesOnQuestionType(questionType) {
+    switch (questionType) {
+        case 'task':
+            return [
+                {
+                    title: 'Learn',
+                    trainingStages: ['learn'],
+                    showForNew: true,
+                    showForOld: false
+                },
+                {
+                    title: 'Train',
+                    trainingStages: ['train', 'trainAfterLearning'],
+                    showForNew: true,
+                    showForOld: true
+                }
+            ];
+        case 'untypedAnswer':
+        case 'typedAnswers':
+            return [
+                {
+                    title: 'Learn',
+                    trainingStages: ['learn'],
+                    showForNew: true,
+                    showForOld: false
+                },
+                {
+                    title: 'Train',
+                    trainingStages: ['train', 'trainAfterLearning'],
+                    showForNew: true,
+                    showForOld: true
+                },
+                {
+                    title: 'Check',
+                    trainingStages: ['check'],
+                    showForNew: true,
+                    showForOld: true
+                }
+            ];
+        default:
+            return [];
+    }
 }
 
 export default QuestionStageSection;
