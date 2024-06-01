766df2dd9eef95b7124d4e44626cbe8eb4ede517
Skorp24 <skorpfiles@gmail.com>
2024-06-01 20:09:16 +0200

    added the first version of loading existing training, fixed a problem with loading stats for questionnaires in loaded trainings

--- src/Pages/ConfigureTraining/ConfigureTrainingPage.jsx
+++ src/Pages/ConfigureTraining/ConfigureTrainingPage.jsx
@@ -1,5 +1,5 @@
 import ConfigureTrainingShell from '../../ConfigureTraining/ConfigureTrainingShell';
-import { useState, useReducer, useEffect } from 'react';
+import { useState, useReducer, useEffect, useCallback, useRef } from 'react';
 import { callApi } from '../../Utils/GlobalUtils';
 import SelectQuestionnairePage from './SelectQuestionnairePage';
 import ReturnToPage from '../../ReturnToPage';
@@ -10,7 +10,153 @@ import LoadingPage from '../LoadingPage';
 
 function ConfigureTrainingPage() {
 
+    //------- Shared objects --------//
     const accessToken = useSelector(state => state.user.accessToken);
+    const defaultValues = {
+        trainingLengthRadioGroup: 'questionsCountRadioButton',
+        trainingName: 'My Training'
+    };
+    const methods = useForm({ defaultValues });
+
+    //------- Calculating Stats -------//
+
+    const questionnairesStatsReducer = (state, action) => {
+        switch (action.type) {
+            case 'setIsLoading':
+                return {
+                    ...state,
+                    isLoading: true,
+                    isFinished: false,
+                    isSuccessful: false,
+                    isError: false,
+                    errorMessage: null
+                };
+            case 'setSuccess':
+                return {
+                    questionnaires: action.questionnaires,
+                    stats: {
+                        questionsTotalCount: action.questionsTotalCount,
+                        newQuestionsCount: action.newQuestionsCount,
+                        recheckedQuestionsCount: action.recheckedQuestionsCount,
+                        maxTimeToTrainMinutes: action.maxTimeToTrainMinutes
+                    },
+                    isLoading: false,
+                    isFinished: true,
+                    isSuccessful: true,
+                    isError: false,
+                    errorMessage: null
+                };
+            case 'setError':
+                return {
+                    ...state,
+                    isLoading: false,
+                    isFinished: true,
+                    isSuccessful: false,
+                    isError: true,
+                    errorMessage: action.errorMessage
+                };
+            default:
+                return { ...state };
+        }
+    }
+
+    const [questionnairesStats, dispatchQuestionnairesStats] = useReducer(questionnairesStatsReducer, {
+        questionnaires: [], //{ id : ..., filled: ..., maxTimeToTrainMinutes : ... }
+        stats: {
+            questionsTotalCount: 0,
+            newQuestionsCount: 0,
+            recheckedQuestionsCount: 0,
+            maxTimeToTrainMinutes: 0
+        },
+        isLoading: false,
+        isFinished: false,
+        isSuccessful: false,
+        isError: false,
+        errorMessage: null
+    });
+
+    const refreshStatsFuncRef = useRef();
+    const refreshStatsFunc = useCallback(async (newTrainingStatusSelectedQuestionnaires) => {
+        try {
+            dispatchQuestionnairesStats({ type: 'setIsLoading' });
+
+            let newQuestionnairesStatsItems = [];
+            let newQuestionsTotalCount = 0;
+            let newNewQuestionsCount = 0;
+            let newRecheckedQuestionsCount = 0;
+
+            //refresh stats items list by adding/deleting absent elements, calculate known values
+            newTrainingStatusSelectedQuestionnaires.forEach(questionnaire => {
+                let newQuestionnaireStats = questionnairesStats.questionnaires.find(q => q.id === questionnaire.id);
+                if (newQuestionnaireStats === undefined) {
+                    newQuestionnaireStats = {
+                        id: questionnaire.id,
+                        filled: false,
+                        totalTrainingTimeSeconds: null
+                    };
+                }
+                newQuestionnairesStatsItems = [
+                    ...newQuestionnairesStatsItems,
+                    newQuestionnaireStats
+                ];
+
+                newQuestionsTotalCount += questionnaire.countsOfQuestions.total;
+                newNewQuestionsCount += questionnaire.countsOfQuestions.new;
+                newRecheckedQuestionsCount += questionnaire.countsOfQuestions.rechecked;
+            });
+
+            //refresh stats for unfilled items
+            const processQuestionnaireStatsItem = async (questionnaireStatsItem) => {
+                if (!questionnaireStatsItem.filled) {
+                    await new Promise(resolve => setTimeout(resolve, 2000));
+                    const response = await callApi(`/Repository/Questionnaire/${questionnaireStatsItem.id}?calculateTime=true`, 'GET', accessToken);
+                    if (response.ok) {
+                        const result = await response.json();
+                        return ({
+                            id: questionnaireStatsItem.id,
+                            filled: true,
+                            totalTrainingTimeSeconds: result.totalTrainingTimeSeconds
+                        });
+                    }
+                    else {
+                        throw new Error('Unable to get questionnaire stats.');
+                    }
+                }
+                else {
+                    return questionnaireStatsItem;
+                }
+            }
+
+            const processQuestionnairesResults = await Promise.all(newQuestionnairesStatsItems.map(processQuestionnaireStatsItem));
+
+            //set state
+
+            let maxTimeToTrainMinutes = processQuestionnairesResults.reduce(((acc, value) => acc + Math.floor(value.totalTrainingTimeSeconds / 60)), 0);
+            if (maxTimeToTrainMinutes < 1) {
+                maxTimeToTrainMinutes = 1;
+            }
+
+            dispatchQuestionnairesStats({
+                type: 'setSuccess',
+                questionnaires: processQuestionnairesResults,
+                questionsTotalCount: newQuestionsTotalCount,
+                newQuestionsCount: newNewQuestionsCount,
+                recheckedQuestionsCount: newRecheckedQuestionsCount,
+                maxTimeToTrainMinutes
+            });
+        }
+        catch (error) {
+            console.log(error);
+            dispatchQuestionnairesStats({
+                type: 'setError',
+                errorMessage: error
+            });
+        }
+    }, [accessToken, questionnairesStats]);
+
+    useEffect(() => {
+        refreshStatsFuncRef.current = refreshStatsFunc;
+    });
 
     //------- Training Status -------//
 
@@ -71,25 +217,28 @@ function ConfigureTrainingPage() {
             dispatchLoadTrainingState('setIsLoading');
             try {
                 const loadingTrainingFunc = async () => {
-                    let url = '/Repository/Training/' + trainingId;
+                    let url = '/Repository/Training/' + trainingId + '?calculateTime=true';
                     const response = await callApi(url, 'GET', accessToken);
                     if (response.ok) {
                         const result = await response.json();
+                        const selectedQuestionnaires = result.questionnaires.map(item => {
+                            return {
+                                id: item.id,
+                                name: item.name,
+                                countsOfQuestions: item.countsOfQuestions
+                            };
+                        });
                         setTrainingStatus({
                             id: result.id,
                             name: result.name,
-                            selectedQuestionnaires: result.questionnaires.map(item => {
-                                return {
-                                    id: item.id,
-                                    name: item.name
-                                };
-                            }),
+                            selectedQuestionnaires,
                             trainingLengthAsQuestionsCount: result.lengthType === 'questionsCount'
                         });
                         methods.setValue('trainingName', result.name);
                         methods.setValue('questionsCount', result.questionsCount);
                         methods.setValue('time', result.timeMinutes);
                         dispatchLoadTrainingState('setSuccess');
+                        await refreshStatsFuncRef.current(selectedQuestionnaires);
                     }
                     else {
                         const result = await response.json();
@@ -103,141 +252,7 @@ function ConfigureTrainingPage() {
                 dispatchLoadTrainingState({ type: 'setError', errorMessage: 'Error: Unable to connect to the API.' });
             }
         }
-    }, [accessToken, searchParams]);
-
-    //------- Calculating Stats -------//
-
-    const questionnairesStatsReducer = (state, action) => {
-        switch (action.type) {
-            case 'setIsLoading':
-                return {
-                    ...state,
-                    isLoading: true,
-                    isFinished: false,
-                    isSuccessful: false,
-                    isError: false,
-                    errorMessage: null
-                };
-            case 'setSuccess':
-                return {
-                    questionnaires: action.questionnaires,
-                    stats: {
-                        questionsTotalCount: action.questionsTotalCount,
-                        newQuestionsCount: action.newQuestionsCount,
-                        recheckedQuestionsCount: action.recheckedQuestionsCount,
-                        maxTimeToTrainMinutes: action.maxTimeToTrainMinutes
-                    },
-                    isLoading: false,
-                    isFinished: true,
-                    isSuccessful: true,
-                    isError: false,
-                    errorMessage: null
-                };
-            case 'setError':
-                return {
-                    ...state,
-                    isLoading: false,
-                    isFinished: true,
-                    isSuccessful: false,
-                    isError: true,
-                    errorMessage: action.errorMessage
-                };
-            default:
-                return { ...state };
-        }
-    }
-
-    const refreshStatsFunc = async (newTrainingStatusSelectedQuestionnaires) => {
-        try {
-            dispatchQuestionnairesStats({ type: 'setIsLoading' });
-
-            let newQuestionnairesStatsItems = [];
-            let newQuestionsTotalCount = 0;
-            let newNewQuestionsCount = 0;
-            let newRecheckedQuestionsCount = 0;
-
-            //refresh stats items list by adding/deleting absent elements, calculate known values
-            newTrainingStatusSelectedQuestionnaires.forEach(questionnaire => {
-                let newQuestionnaireStats = questionnairesStats.questionnaires.find(q => q.id === questionnaire.id);
-                if (newQuestionnaireStats === undefined) {
-                    newQuestionnaireStats = {
-                        id: questionnaire.id,
-                        filled: false,
-                        totalTrainingTimeSeconds: null
-                    };
-                }
-                newQuestionnairesStatsItems = [
-                    ...newQuestionnairesStatsItems,
-                    newQuestionnaireStats
-                ];
-
-                newQuestionsTotalCount += questionnaire.countsOfQuestions.total;
-                newNewQuestionsCount += questionnaire.countsOfQuestions.new;
-                newRecheckedQuestionsCount += questionnaire.countsOfQuestions.rechecked;
-            });
-
-            //refresh stats for unfilled items
-            const processQuestionnaireStatsItem = async (questionnaireStatsItem) => {
-                if (!questionnaireStatsItem.filled) {
-                    const response = await callApi(`/Repository/Questionnaire/${questionnaireStatsItem.id}?calculateTime=true`, 'GET', accessToken);
-                    if (response.ok) {
-                        const result = await response.json();
-                        return ({
-                            id: questionnaireStatsItem.id,
-                            filled: true,
-                            totalTrainingTimeSeconds: result.totalTrainingTimeSeconds
-                        });
-                    }
-                    else {
-                        throw new Error('Unable to get questionnaire stats.');
-                    }
-                }
-                else {
-                    return questionnaireStatsItem;
-                }
-            }
-
-            const processQuestionnairesResults = await Promise.all(newQuestionnairesStatsItems.map(processQuestionnaireStatsItem));
-
-            //set state
-
-            let maxTimeToTrainMinutes = processQuestionnairesResults.reduce(((acc, value) => acc + Math.floor(value.totalTrainingTimeSeconds / 60)), 0);
-            if (maxTimeToTrainMinutes < 1) {
-                maxTimeToTrainMinutes = 1;
-            }
-
-            dispatchQuestionnairesStats({
-                type: 'setSuccess',
-                questionnaires: processQuestionnairesResults,
-                questionsTotalCount: newQuestionsTotalCount,
-                newQuestionsCount: newNewQuestionsCount,
-                recheckedQuestionsCount: newRecheckedQuestionsCount,
-                maxTimeToTrainMinutes
-            });
-        }
-        catch (error) {
-            console.log(error);
-            dispatchQuestionnairesStats({
-                type: 'setError',
-                errorMessage: error
-            });
-        }
-    }
-
-    const [questionnairesStats, dispatchQuestionnairesStats] = useReducer(questionnairesStatsReducer, {
-        questionnaires: [], //{ id : ..., filled: ..., maxTimeToTrainMinutes : ... }
-        stats: {
-            questionsTotalCount: 0,
-            newQuestionsCount: 0,
-            recheckedQuestionsCount: 0,
-            maxTimeToTrainMinutes: 0
-        },
-        isLoading: false,
-        isFinished: false,
-        isSuccessful: false,
-        isError: false,
-        errorMessage: null
-    });
+    }, [accessToken, methods, searchParams]);
 
     //------- Configure Page / Select Questionnaire Toggling -------//
 
@@ -249,13 +264,6 @@ function ConfigureTrainingPage() {
     });
 
     //------- UI filling and handling -------//
-
-    const defaultValues = {
-        trainingLengthRadioGroup: 'questionsCountRadioButton',
-        trainingName: 'My Training'
-    };
-
-    const methods = useForm({defaultValues});
 
     const handleAddingAnotherQuestionnaire = () => setSelectQuestionnairePageIsShown(true);
     const handleDeleteQuestionnaire = async (id) => {
